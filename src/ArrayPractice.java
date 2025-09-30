59a01611158de9517a605eff28a7325cd20b0c0b
allenres <allen.resulidze@gmail.com>
2025-09-30 14:20:29 -0700

    initialize string array

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -1,9 +1,13 @@
 public class ArrayPractice {
   public static void main(String[] args) {
     // Create an array of Strings of size 4
-
+    String[] array = new String[4];
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
+    array[0] = "a";
+    array[1] = "b";
+    array[2] = "c";
+    array[3] = "d";
 
     // Get the value of the array at index 2
 
